a36a27bc5b96fd30ddc2fcb3236c261be4e12759
Spider-Man <abeernaeem6@gmail.com>
2023-10-22 18:50:58 -0500

    updated Header styling plus searching functinality

--- src/Components/Desktop/Header/Header.js
+++ src/Components/Desktop/Header/Header.js
@@ -2,30 +2,32 @@ import Link from "next/link";
 import { AiFillCar } from "react-icons/ai";
 import { BsPhone } from "react-icons/bs";
 import { MdOutlineMailOutline } from "react-icons/md";
-import './Header.css';
+import "./Header.css";
 
 export default function Header() {
   return (
-    <main style={{ "backgroundColor": "powderblue", "height" : "5vh" }}>
+    <main style={{ backgroundColor: "powderblue", height: "5vh" }}>
       <div className="container-fluid text-center align-middle fs-5">
         <div className="row">
-          <div className="col-6">
-            <span style={{"marginRight" : "1em"}}>123 Imaginary Ln, City State, 123456 </span>
-            <AiFillCar style={{"marginRight" : ".3em"}} />
+          <div className="col-6 d-flex align-items-center  ">
+            <span className="text">123 Imaginary Ln, City State, 123456 </span>
+            <AiFillCar className="logo " style={{ marginRight: ".3em" }} />
             <a
               className="directions"
               href="https://www.google.com/maps/place/Bermuda+Triangle/@24.9999945,-71.0389691,14z/data=!3m1!4b1!4m6!3m5!1s0x89451ab5034cb7ab:0xb600ecf3df7aca4d!8m2!3d25!4d-71!16zL20vMDFqcGw?entry=ttu"
-
-              
             >
-              <span className="fs-5" style={{"margin" : ".5em"}}>Get Directions</span>
+              <span className="text" style={{ margin: ".5em" }}>
+                Get Directions
+              </span>
             </a>
           </div>
-          <div className="col-3">
-            <BsPhone className="logo" /> XXX-XXX-XXXX{" "}
+          <div className="col-3 d-flex align-items-center">
+            <BsPhone className="text" />{" "}
+            <span className="text">XXX-XXX-XXXX</span>
           </div>
-          <div className="col-3">
-            <MdOutlineMailOutline className="logo" /> Email@email.com
+          <div className="col-3 d-flex align-items-center">
+            <MdOutlineMailOutline className="logo" />{" "}
+            <span className="text">Email@email.com</span>
           </div>
         </div>
       </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,46 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Link from 'next/link'
+"use client";
+
+import React, { useEffect, useState } from "react";
+import "./page.css";
 
 export default function Home() {
-  return (
-    <main >
+  const Corolla = [
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.roadandtrack.com%2Fcar-culture%2Fbuying-maintenance%2Fg6710%2Fcheap-sports-cars%2F&psig=AOvVaw31op39zUC48Y0e95NcUVOW&ust=1697853571179000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCODjz8nDg4IDFQAAAAAdAAAAABAE",
+    "'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fphotos%2Fcar&psig=AOvVaw31op39zUC48Y0e95NcUVOW&ust=1697853571179000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCODjz8nDg4IDFQAAAAAdAAAAABAJ",
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.mynrma.com.au%2Fcars-and-driving%2Fbuying-a-car%2Ffeatures%2Fbest-cars-2021&psig=AOvVaw31op39zUC48Y0e95NcUVOW&ust=1697853571179000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCODjz8nDg4IDFQAAAAAdAAAAABAi",
+  ];
+  console.log(Corolla);
+  let count = 0;
+  const [car, setCar] = useState(Corolla[count]);
+  const [zoomE, setzoomE] = useState(false);
 
-      <h1>Header</h1>
-      <div class="card" style={{'width': '18rem'}}>
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" class="btn btn-primary">
-            Go somewhere
-          </a>
+  const handleChange = () => {
+    setCar(Corolla[count + 1]);
+    if (count === Corolla.length - 2) {
+      count = -1;
+    } else {
+      count = count + 1;
+    }
+
+    console.log(count);
+  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      handleChange();
+    }, 5010);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      <div className="overflow">
+        <div key={{ car }} className="slideshow" style={{ width: "75%" }}>
+          <h1>{car}</h1>
+          <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.roadandtrack.com%2Fcar-culture%2Fbuying-maintenance%2Fg6710%2Fcheap-sports-cars%2F&psig=AOvVaw31op39zUC48Y0e95NcUVOW&ust=1697853571179000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCODjz8nDg4IDFQAAAAAdAAAAABAE"  alt=""></img>          
+          <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.roadandtrack.com%2Fcar-culture%2Fbuying-maintenance%2Fg6710%2Fcheap-sports-cars%2F&psig=AOvVaw31op39zUC48Y0e95NcUVOW&ust=1697853571179000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCODjz8nDg4IDFQAAAAAdAAAAABAE" className="slideshowPic zoom" alt=""></img>
+          <h1>Test</h1>
         </div>
       </div>
-<Link href={'/notes'} >
-      <h1>Hellow World </h1>
-      </Link>
-    </main>
-  )
+    </>
+  );
 }
